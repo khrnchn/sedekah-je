e8ba803c47a932521c39d0f9e9ea68049f251fae
alserembani94 <alserembani94@gmail.com>
2024-08-18 02:15:31 +0800

    feat: adding qrContent

--- app/types/index.d.ts
+++ app/types/index.d.ts
@@ -1,5 +1,12 @@
+type PaymentOption = "duitnow" | 'tng';
+
 type Mosque = {
+	id: number;
 	name: string;
 	location: string;
 	image: string;
+
+	// Just a suggestion, tapi ni require extra work, untuk extract dulu info dari QR code, so boleh restructure QR for high definition
+	qrContent?: string;
+	supportedPayment?: PaymentOption[];
 };
