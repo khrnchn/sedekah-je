0d94f4c59dc54770240f526adbc61beef98fa22b
khairin <khairin13chan@gmail.com>
2024-08-17 22:30:14 +0800

    :lemon: add mosques qr

--- app/data/mosques.ts
+++ app/data/mosques.ts
@@ -1,32 +1,88 @@
 export const mosques: Mosque[] = [
-	{
-		name: "Masjid Negara",
-		location: "Kuala Lumpur",
-		image: "/masjid/masjid-negara.png",
-	},
-	{
-		name: "Masjid Al Ghufran",
-		location: "-",
-		image: "/masjid/masjid-al-ghufran.png",
-	},
-	{
-		name: "Masjid Al Muhtadin",
-		location: "Selangor",
-		image: "/masjid/masjid-al-muhtadin.png",
-	},
-	{
-		name: "Masjid Ar Rahman",
-		location: "Kuala Lumpur",
-		image: "/masjid/masjid-ar-rahman.png",
-	},
-	{
-		name: "Masjid Lama Kampung Sura MLKS",
-		location: "Terengganu",
-		image: "/masjid/masjid-lama-kampung-sura-mlks.png",
-	},
-	{
-		name: "Surau As Salam Seksyen 4",
-		location: "Selangor",
-		image: "/surau/surau-as-salam-seksyen-4.png",
-	},
+  {
+    name: "Masjid Negara",
+    location: "Kuala Lumpur",
+    image: "/masjid/masjid-negara.png",
+  },
+  {
+    name: "Masjid Al Ghufran",
+    location: "-",
+    image: "/masjid/masjid-al-ghufran.png",
+  },
+  {
+    name: "Masjid Al Muhtadin",
+    location: "Selangor",
+    image: "/masjid/masjid-al-muhtadin.png",
+  },
+  {
+    name: "Masjid Ar Rahman",
+    location: "Kuala Lumpur",
+    image: "/masjid/masjid-ar-rahman.png",
+  },
+  {
+    name: "Masjid Lama Kampung Sura MLKS",
+    location: "Terengganu",
+    image: "/masjid/masjid-lama-kampung-sura-mlks.png",
+  },
+  {
+    name: "Surau As Salam Seksyen 4",
+    location: "Selangor",
+    image: "/surau/surau-as-salam-seksyen-4.png",
+  },
+  ,
+  {
+    name: "Masjid Sri Sendayan",
+    location: "Negeri Sembilan",
+    image: "/masjid/masjid-sri-sendayan.png",
+  },
+  {
+    name: "Masjid Putra Heights",
+    location: "Selangor",
+    image: "/masjid/masjid-putra-heights.png",
+  },
+  {
+    name: "Masjid Azzubair Ibnul Awwam",
+    location: "Kuala Lumpur",
+    image: "/masjid/masjid-azzubair-ibnul-awwam.jpeg",
+  },
+  {
+    name: "Masjid Lestari Putra",
+    location: "Selangor",
+    image: "/masjid/masjid-lestari-putra.jpeg",
+  },
+  {
+    name: "Masjid Bandar Saujana Putra",
+    location: "Selangor",
+    image: "/masjid/masjid-bandar-saujana-putra.jpeg",
+  },
+  {
+    name: "Masjid Al Ansar",
+    location: "Kuala Lumpur",
+    image: "/masjid/masjid-al-ansar.jpeg",
+  },
+  {
+    name: "Masjid Tuminah Hamidi",
+    location: "Perak",
+    image: "/masjid/masjid-tuminah-hamidi.jpeg",
+  },
+  {
+    name: "Rumah Bakti Nur Ain",
+    location: "Selangor",
+    image: "/lain/rumah-bakti-nur-ain.jpeg",
+  },
+  {
+    name: "Masjid Jamek Al Manar",
+    location: "Johor",
+    image: "/masjid/masjid-jamek-al-manar.jpeg",
+  },
+  {
+    name: "Masjid Tengku Ampuan Afzan",
+    location: "Pahang",
+    image: "/masjid/masjid-tengku-ampuan-afzan.jpeg",
+  },
+  {
+    name: "Masjid UTM KL",
+    location: "Kuala Lumpur",
+    image: "/masjid/masjid-utmkl.jpeg",
+  },
 ];
