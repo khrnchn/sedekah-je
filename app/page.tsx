3bd673f27c8de9dcd6778deb31a46edf703dff85
altafxx <altafhasnan@gmail.com>
2024-08-18 22:52:44 +0800

    feat: added map with partial data

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@
 import { Header } from "@/components/ui/header";
 import { SearchBar } from "@/components/ui/searchbar";
 import type React from "react";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { institutions } from "./data/institutions";
 import InstitutionCard from "@/components/ui/institution-card";
 import Filters from "@/components/sections/filters";
+import dynamic from "next/dynamic";
 
 const Home: React.FC = () => {
 	const [query, setQuery] = useState<string>("");
@@ -20,18 +21,27 @@ const Home: React.FC = () => {
 		setSelectedCategories(props.categories);
 	};
 
+	const Map = useMemo(() => dynamic(
+		() => import('@/components/map'),
+		{
+			loading: () => <p>A map is loading</p>,
+			ssr: false
+		}
+	), [])
+
 	const filteredInstitutions = institutions.filter((institution) => {
 		const lowercaseQuery = query.toLowerCase();
 		return (
 			institution.name.toLowerCase().includes(lowercaseQuery) &&
 			(selectedCategories.length === 0 ||
-			selectedCategories.includes(institution.category))
+				selectedCategories.includes(institution.category))
 		);
 	});
 
 	return (
 		<div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8 flex flex-col gap-8">
 			<Header />
+			<Map />
 			<Filters onChange={handleFilters} />
 			<SearchBar onSearch={handleSearch} />
 			<div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
