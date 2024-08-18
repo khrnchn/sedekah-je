de70d5cc6d7f447b767f6fe88600cf3cd7015572
khairin <khairin13chan@gmail.com>
2024-08-19 02:10:52 +0800

    :joy: change language to malay

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -2,7 +2,7 @@ export const Header = () => (
   <div className="text-center mb-8">
     <h2 className="text-3xl font-bold mb-2 text-green-600">SedekahJe</h2>
     <p className="text-lg text-gray-600">
-      Twitter crowdsourced list of mosques/surau QRs in Malaysia.
+      Senarai QR masjid/ surau/ institusi yang dikumpulkan oleh netizen di X.
     </p>
   </div>
 );
