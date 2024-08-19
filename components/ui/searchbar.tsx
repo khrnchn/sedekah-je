481d0add6c7e3330a198be8269b44fe11580640d
Fikri <fikfan@gmail.com>
2024-08-19 21:41:26 +0800

    added debounce function for search, installed lodash-es for debounce

--- components/ui/searchbar.tsx
+++ components/ui/searchbar.tsx
@@ -1,16 +1,27 @@
 "use client";
-
+import { useState } from "react";
 import { Input } from "@/components/ui/input";
 
 interface SearchBarProps {
   onSearch: (query: string) => void;
 }
 
-export const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => (
-  <Input
-    type="search"
-    placeholder="Cari masjid/ surau/ institusi..."
-    className="w-full rounded-lg bg-muted pl-8 pr-4 py-2 text-sm"
-    onChange={(e) => onSearch(e.target.value)}
-  />
-);
+export const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setSearchTerm(newValue);
+    onSearch(newValue);
+  };
+
+  return (
+    <Input
+      type="search"
+      placeholder="Cari masjid/ surau/ institusi..."
+      className="w-full rounded-lg bg-muted pl-8 pr-4 py-2 text-sm"
+      value={searchTerm}
+      onChange={handleInputChange}
+    />
+  );
+};
