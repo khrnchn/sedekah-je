fa542e9b07c5fadbd7b8efec4a3449d6dafd02a9
Haziq Khairi <ikhairulhaziq11@gmail.com>
2024-08-20 14:36:28 +0800

    ✨ feat: add notifications for using `shadcn`'s sonner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/toaster"; 
+import { Toaster } from "@/components/ui/sonner";
 import { Analytics } from '@vercel/analytics/react';
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +21,7 @@ export default function RootLayout({
 			<body className={inter.className}>
 				{children}
 				<Analytics />
-				<Toaster />
+				<Toaster richColors/>
 			</body>
 		</html>
 	);
