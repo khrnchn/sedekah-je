eb19c01ce2b5f977f2edc2c6f5c3dff1eda8033e
Nero One <info@nero1.net>
2024-08-27 09:11:30 +0800

    feat: use slug instead of id for routing

---
+++ app/[institution]/[slug]/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import PageSection from "@/components/ui/pageSection";
+import type React from "react";
+import { institutions } from "@/app/data/institutions";
+import InstitutionCard from "@/components/ui/institution-card";
+import CustomMap from "../../../components/custom-map";
+import { CategoryColor } from "../../types/institutions";
+import { notFound } from "next/navigation";
+import { slugify } from "@/lib/utils";
+
+type Params = {
+  params: {
+    slug: string;
+  };
+};
+
+const InstitutionPage: React.FC<Params> = ({ params }) => {
+  const institution = institutions.find(
+    (institution) => slugify(institution.name) === params.slug,
+  );
+
+  if (!institution) {
+    notFound();
+  }
+
+  return (
+    <PageSection>
+      <div className="space-y-4">
+        {institution.coords?.length && (
+          <CustomMap
+            marker={{
+              coords: institution.coords,
+              name: institution.name,
+              color: CategoryColor[institution.category],
+            }}
+          />
+        )}
+        <InstitutionCard key={institution.id} {...institution} />
+      </div>
+    </PageSection>
+  );
+};
+
+export default InstitutionPage;
--- components/share-to-x.tsx
+++ components/share-to-x.tsx
@@ -1,38 +1,46 @@
-"use client"
+"use client";
 
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function ShareToX({ data, imageUrl }: { data: { category: string, id: number, name: string }, imageUrl?: string }) {
-    const baseUrl = window.location.origin;
+import { slugify } from "@/lib/utils";
 
-    //   const handleShareClick = async () => {
-    //     //TODO: Implement share to whatsapp with QR instead of url
+export default function ShareToX({
+  data,
+  imageUrl,
+}: {
+  data: { category: string; id: number; name: string };
+  imageUrl?: string;
+}) {
+  const baseUrl = window.location.origin;
 
-    //     const response = await fetch(imageUrl);
-    //     const blob = await response.blob();
+  //   const handleShareClick = async () => {
+  //     //TODO: Implement share to whatsapp with QR instead of url
 
-    //     const reader = new FileReader();
-    //     reader.readAsDataURL(blob);
-    //     reader.onload = async () => {
-    //       const imageDataURI = reader.result as string;
-    //       const message = `Sumbang ke institusi ...! ${imageDataURI}`;
-    //       const whatsappLink = `whatsapp://send?text=${encodeURIComponent(message)}`;
-    //       window.location.href = whatsappLink;
-    //     };
-    //   };
+  //     const response = await fetch(imageUrl);
+  //     const blob = await response.blob();
 
-    const message = `Sumbang ke ${data.name}!`;
-    const shareUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(`${message}\n${baseUrl}/${data.category}/${data.id}`)}`;
+  //     const reader = new FileReader();
+  //     reader.readAsDataURL(blob);
+  //     reader.onload = async () => {
+  //       const imageDataURI = reader.result as string;
+  //       const message = `Sumbang ke institusi ...! ${imageDataURI}`;
+  //       const whatsappLink = `whatsapp://send?text=${encodeURIComponent(message)}`;
+  //       window.location.href = whatsappLink;
+  //     };
+  //   };
 
-    return (
-        <a target="_blank" href={shareUrl} rel="noopener noreferrer">
-            Kongsi ke 𝕏
-        </a>
-        // <button onClick={handleShareClick}>
-        //   Share with WhatsApp
-        //   <Image src={imageUrl} alt="Share Image" width={50} height={50} />
-        // </button>
-    );
-};
+  const message = `Sumbang ke ${data.name}!`;
+  const shareUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(`${message}\n${baseUrl}/${data.category}/${slugify(data.name)}`)}`;
+
+  return (
+    <a target="_blank" href={shareUrl} rel="noopener noreferrer">
+      Kongsi ke 𝕏
+    </a>
+    // <button onClick={handleShareClick}>
+    //   Share with WhatsApp
+    //   <Image src={imageUrl} alt="Share Image" width={50} height={50} />
+    // </button>
+  );
+}
