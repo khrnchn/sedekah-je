4468518d37f6fbda1514d878ddd62727bdf323af
Haziq Khairi <ikhairulhaziq11@gmail.com>
2024-08-27 14:28:23 +0800

    ✨ Add state flags

--- components/states-dropdown.tsx
+++ components/states-dropdown.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import * as React from "react";
+import Image from "next/image";
 import { cn } from "@/lib/utils";
 import {
 	Select,
@@ -15,23 +16,39 @@ import {
 const ALL_STATES = "all_states";
 
 const states = [
-	{ label: "Semua Negeri", value: ALL_STATES },
-	{ label: "Johor", value: "Johor" },
-	{ label: "Kedah", value: "Kedah" },
-	{ label: "Kelantan", value: "Kelantan" },
-	{ label: "Melaka", value: "Melaka" },
-	{ label: "Negeri Sembilan", value: "Negeri Sembilan" },
-	{ label: "Pahang", value: "Pahang" },
-	{ label: "Perak", value: "Perak" },
-	{ label: "Perlis", value: "Perlis" },
-	{ label: "Pulau Pinang", value: "Pulau Pinang" },
-	{ label: "Sabah", value: "Sabah" },
-	{ label: "Sarawak", value: "Sarawak" },
-	{ label: "Selangor", value: "Selangor" },
-	{ label: "Terengganu", value: "Terengganu" },
-	{ label: "W.P. Kuala Lumpur", value: "W.P. Kuala Lumpur" },
-	{ label: "W.P. Labuan", value: "W.P. Labuan" },
-	{ label: "W.P. Putrajaya", value: "W.P. Putrajaya" },
+	{ label: "Semua Negeri", value: ALL_STATES, flag: null },
+	{ label: "Johor", value: "Johor", flag: "/flags/Johor.svg" },
+	{ label: "Kedah", value: "Kedah", flag: "/flags/Kedah.svg" },
+	{ label: "Kelantan", value: "Kelantan", flag: "/flags/Kelantan.svg" },
+	{ label: "Melaka", value: "Melaka", flag: "/flags/Melaka.svg" },
+	{
+		label: "Negeri Sembilan",
+		value: "Negeri Sembilan",
+		flag: "/flags/Negeri-Sembilan.svg",
+	},
+	{ label: "Pahang", value: "Pahang", flag: "/flags/Pahang.svg" },
+	{ label: "Perak", value: "Perak", flag: "/flags/Perak.svg" },
+	{ label: "Perlis", value: "Perlis", flag: "/flags/Perlis.svg" },
+	{
+		label: "Pulau Pinang",
+		value: "Pulau Pinang",
+		flag: "/flags/Pulau-Pinang.svg",
+	},
+	{ label: "Sabah", value: "Sabah", flag: "/flags/Sabah.svg" },
+	{ label: "Sarawak", value: "Sarawak", flag: "/flags/Sarawak.svg" },
+	{ label: "Selangor", value: "Selangor", flag: "/flags/Selangor.svg" },
+	{ label: "Terengganu", value: "Terengganu", flag: "/flags/Terengganu.svg" },
+	{
+		label: "W.P. Kuala Lumpur",
+		value: "W.P. Kuala Lumpur",
+		flag: "/flags/WP-Kuala-Lumpur.svg",
+	},
+	{ label: "W.P. Labuan", value: "W.P. Labuan", flag: "/flags/WP-Labuan.svg" },
+	{
+		label: "W.P. Putrajaya",
+		value: "W.P. Putrajaya",
+		flag: "/flags/WP-Putrajaya.svg",
+	},
 ];
 
 type Props = {
@@ -54,10 +71,26 @@ export function StatesDropdown({ onChange, className }: Props) {
 			</SelectTrigger>
 			<SelectContent>
 				<SelectGroup>
-					<SelectLabel>Senarai Negeri</SelectLabel>
 					{states.map((state) => (
-						<SelectItem key={state.value} value={state.value}>
-							{state.label}
+						<SelectItem
+							key={state.value}
+							value={state.value}
+							className="flex items-center"
+						>
+							{state.flag ? (
+								<div className="flex items-center space-x-3 w-full">
+									<Image
+										src={state.flag}
+										alt={`${state.label} flag`}
+										width={20}
+										height={12}
+										className="object-contain"
+									/>
+									<span>{state.label}</span>
+								</div>
+							) : (
+								<span>{state.label}</span>
+							)}
 						</SelectItem>
 					))}
 				</SelectGroup>
