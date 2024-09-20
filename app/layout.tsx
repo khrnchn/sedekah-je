c540bb452ed82645c328c5eadc3d7edc473d1184
Farhan Helmy <farhanhlmy@gmail.com>
2024-09-20 21:00:19 +0800

    :sparkles: Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,76 +7,82 @@ import Ribbon from "@/components/ui/ribbon";
 import { Toaster } from "@/components/ui/sonner";
 import { cn } from "@/lib/utils";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 const poppins = Poppins({ weight: ["400", "700", "900"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Sedekah Je",
-	description:
-		"Curated and crowdsourced list of mosques/surau/institution QR codes in Malaysia",
-	keywords: [
-		"sedekahje",
-		"sedekah je",
-		"sedekah qr",
-		"sedekah je qr",
-		"opensource sedekah qr",
-		"sedekah malaysia",
-		"sedekah malaysia qr",
-		"sedekah malaysia qr codes",
-		"senarai qr sedekah malaysia",
-		"qr sedekah malaysia",
-		"qr code sedekah malaysia",
-	],
-	metadataBase: new URL("https://sedekahje.com"),
-	openGraph: {
-		type: "website",
-		url: "https://sedekahje.com",
-		title: "Sedekah Je",
-		description:
-			"Curated and crowdsourced list of mosques/surau/institution QR codes in Malaysia",
-		siteName: "Sedekah Je",
-		images: "https://sedekahje.com/sedekahje-og.png",
-	},
-	twitter: {
-		card: "summary_large_image",
-		site: "@asdfghjkhairin",
-		creator: "@asdfghjkhairin",
-		title: "Sedekah Je",
-		description:
-			"Curated and crowdsourced list of mosques/surau/institution QR codes in Malaysia",
-		images: "https://sedekahje.com/sedekahje-twitter.png",
-	},
+  title: "Sedekah Je",
+  description:
+    "Curated and crowdsourced list of mosques/surau/institution QR codes in Malaysia",
+  keywords: [
+    "sedekahje",
+    "sedekah je",
+    "sedekah qr",
+    "sedekah je qr",
+    "opensource sedekah qr",
+    "sedekah malaysia",
+    "sedekah malaysia qr",
+    "sedekah malaysia qr codes",
+    "senarai qr sedekah malaysia",
+    "qr sedekah malaysia",
+    "qr code sedekah malaysia",
+  ],
+  metadataBase: new URL("https://sedekahje.com"),
+  openGraph: {
+    type: "website",
+    url: "https://sedekahje.com",
+    title: "Sedekah Je",
+    description:
+      "Curated and crowdsourced list of mosques/surau/institution QR codes in Malaysia",
+    siteName: "Sedekah Je",
+    images: "https://sedekahje.com/sedekahje-og.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@asdfghjkhairin",
+    creator: "@asdfghjkhairin",
+    title: "Sedekah Je",
+    description:
+      "Curated and crowdsourced list of mosques/surau/institution QR codes in Malaysia",
+    images: "https://sedekahje.com/sedekahje-twitter.png",
+  },
 };
 
 export const viewport: Viewport = {
-	initialScale: 1,
-	width: "device-width",
-	maximumScale: 1,
-	viewportFit: "cover",
+  initialScale: 1,
+  width: "device-width",
+  maximumScale: 1,
+  viewportFit: "cover",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<body
-				className={cn(
-					"bg-background transition-colors duration-200 ease-in-out overscroll-y-none",
-					poppins.className,
-				)}
-			>
-				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-					<Header />
-					<div className="h-24 w-full absolute top-0 left-0 bg-gradient-to-b from-orange-300 to-background -z-10 opacity-20 dark:opacity-5" />
-					{children}
-					<Analytics />
-					<Toaster richColors />
-					{/* <Ribbon /> */}
-				</ThemeProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <Script
+        defer
+        src="https://analytics.farhanhelmy.com/script.js"
+        data-website-id="c2f79734-cbe5-4b9d-afd0-75e063e0aadb"
+      />
+      <body
+        className={cn(
+          "bg-background transition-colors duration-200 ease-in-out overscroll-y-none",
+          poppins.className
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Header />
+          <div className="h-24 w-full absolute top-0 left-0 bg-gradient-to-b from-orange-300 to-background -z-10 opacity-20 dark:opacity-5" />
+          {children}
+          <Analytics />
+          <Toaster richColors />
+          {/* <Ribbon /> */}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
