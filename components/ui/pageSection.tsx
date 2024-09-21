9bd70ded2dc444cfe0f52a8a9b28cf4dd1f02935
Haziq Khairi <ihaziqkhairi.dev@gmail.com>
2024-09-21 22:09:17 +0800

    style: adjust padding
    - change horizontal padding from px-2 to px-4 for mobile screens
    - add bottom padding

--- components/ui/pageSection.tsx
+++ components/ui/pageSection.tsx
@@ -14,7 +14,8 @@ const PageSection = forwardRef(
 			<section
 				ref={ref}
 				className={cn(
-					"max-w-5xl mx-auto px-2 sm:px-4 lg:px-6 flex flex-col gap-4 sm:gap-6 lg:gap-8",
+					"max-w-5xl mx-auto px-4 lg:px-6 flex flex-col gap-4 sm:gap-6 lg:gap-8",
+					"pb-8 sm:pb-12 lg:pb-16", // Add bottom padding
 					className,
 				)}
 				{...rest}
