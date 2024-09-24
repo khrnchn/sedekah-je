5ba04cd5688a0e36671355aec4956b1f656f605b
Kyziq <ihaziqkhairi.dev@gmail.com>
2024-09-24 14:36:14 +0800

    feat: update map component
    - Add fullscreen
    - Add ability to locate user
    - Improve DX
    - Sort imports
    - Remove unused and redundant code

--- app/page.tsx
+++ app/page.tsx
@@ -128,7 +128,7 @@ const Home = () => {
 				</div>
 			</div>
 
-			<CollapsibleCustomMap showAll={true} />
+			<CollapsibleCustomMap />
 
 			{isLoading ? (
 				<div className="grid gap-4 sm:gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
