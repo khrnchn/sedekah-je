b4003286b4af677d3bd0e757e17fc4c610e61afb
Kyziq <ihaziqkhairi.dev@gmail.com>
2024-09-24 14:20:29 +0800

    feat(types): add declarations for Leaflet locate and fullscreen controls
    To fix TS errors
    - Property 'locate' does not exist on type 'typeof control'.ts(2339)
    - Property 'fullscreen' does not exist on type 'typeof control'.ts(2339)

---
+++ app/types/leaflet-extensions.d.ts
@@ -0,0 +1,33 @@
+import 'leaflet';
+
+declare module 'leaflet' {
+  namespace Control {
+    interface LocateOptions extends L.ControlOptions {
+      strings?: {
+        title: string;
+      };
+      locateOptions?: L.LocateOptions;
+    }
+
+    class Locate extends L.Control {
+      constructor(options?: LocateOptions);
+    }
+
+    interface FullscreenOptions extends L.ControlOptions {
+      title?: string;
+      titleCancel?: string;
+      forceSeparateButton?: boolean;
+      forcePseudoFullscreen?: boolean;
+      fullscreenElement?: HTMLElement;
+    }
+
+    class Fullscreen extends L.Control {
+      constructor(options?: FullscreenOptions);
+    }
+  }
+
+  namespace control {
+    function locate(options?: Control.LocateOptions): Control.Locate;
+    function fullscreen(options?: Control.FullscreenOptions): Control.Fullscreen;
+  }
+}
