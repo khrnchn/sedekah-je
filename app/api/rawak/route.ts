8b8f2c39e2c952e687568425f358666a0cf4a0e4
Afrie Irham <afrieirham.work@gmail.com>
2024-09-27 09:35:34 +0800

    add /qr/:slug page for QR display

--- app/api/rawak/route.ts
+++ app/api/rawak/route.ts
@@ -1,10 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
-import slugify from "slugify";
+import puppeteer from "puppeteer-core";
 
 import { institutions } from "@/app/data/institutions";
+import { slugify } from "@/lib/utils";
 
 export async function POST(req: NextRequest) {
+  const baseUrl = "http://sedekahje.com";
+  const random = Math.floor(Math.random() * (institutions.length + 1));
+
   const { botToken, chatId } = (await req.json()) as {
     botToken?: string;
     chatId?: string;
@@ -14,17 +18,25 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ status: "not ok" });
   }
 
-  const position = Math.floor(Math.random() * (institutions.length + 1));
-  const photoSlug = `qr-${slugify(String(institutions[position].name), {
-    lower: true,
-    strict: true,
-  })}.png`;
+  // generate screenshot from /qr/:slug
+  const browser = await puppeteer.launch({ channel: "chrome" });
+  const page = await browser.newPage();
+  await page.goto(`${baseUrl}/qr/${slugify(institutions[random].name)}`, {
+    waitUntil: "networkidle2",
+  });
+  const base64Screenshot = await page.screenshot({ encoding: "base64" });
+  await browser.close();
 
-  // feel free to fork and change this
-  const photoUrl = `https://raw.githubusercontent.com/afrieirham/sedekahje-qr-generator/refs/heads/main/QR/${photoSlug}`;
-  await fetch(
-    `https://api.telegram.org/bot${botToken}/sendPhoto?chat_id=${chatId}&photo=${photoUrl}`
-  );
+  // convert to buffer
+  const buffer = Buffer.from(base64Screenshot, "base64");
+  const formData = new FormData();
+  formData.append("chat_id", chatId);
+  formData.append("photo", new Blob([buffer], { type: "image/png" }));
+
+  await fetch(`https://api.telegram.org/bot${botToken}/sendPhoto`, {
+    method: "POST",
+    body: formData,
+  });
 
   return NextResponse.json({ status: "ok" });
 }
