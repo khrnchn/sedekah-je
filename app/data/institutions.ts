e008391dad3cf9ace5681daec57f637b8c3da4cd
Black Kaiser <71305935+khrnchn@users.noreply.github.com>
2024-11-08 14:55:39 +0800

    Update app/data/institutions.ts

--- app/data/institutions.ts
+++ app/data/institutions.ts
@@ -8854,7 +8854,7 @@ export const institutions: Institution[] = [
     qrContent: "00020201021126420014A000000615000101066033460210MD000046635204556153034585802MY5921SURAU TENAGA NASIONAL6002MY62530325163704396056300357405129705201637225399907004971063044AB9",
     supportedPayment: ["duitnow", "tng"]
   },
-  // [put your name here]
+  // sitisarahaqilah, github pr - https://github.com/khrnchn/sedekah-je/pull/225
   {
     id: 770,
     name: "Madrasah As-Salam",
