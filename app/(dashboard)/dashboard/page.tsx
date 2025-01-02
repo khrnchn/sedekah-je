ffee81b4eaa187961cd73b9adeae8d075d5f3230
Black Kaiser <khairinchan@bloomthis.co>
2025-01-02 13:04:43 +0800

    fix: 🐛 dashboard page layout

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,31 +1,34 @@
-import { AppSidebar } from "@/components/app-sidebar";
-import { DashboardHeader } from "@/components/dashboard/sj-breadcrumbs";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { SJBreadcrumbs } from "@/components/dashboard/sj-breadcrumbs";
+import { SJHeader } from "@/components/dashboard/sj-header";
+import { LayoutBody, SJLayout } from "@/components/dashboard/sj-layout";
 import { Hello } from "./_components/hello";
 import Stats from "./_components/stats";
 
 export default function Page() {
-	return (
-		<SidebarProvider>
-			<AppSidebar />
-			<SidebarInset>
-				<DashboardHeader
-					currentPage="Home"
-					parentPage={{
-						title: "Dashboard",
-						href: "#",
-					}}
-				/>
-				<div className="container mx-auto p-4">
-					<div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-						<Hello />
-						<Stats />
+  return (
+    <SJLayout>
+      <SJBreadcrumbs
+        currentPage="List of Institutions"
+        parentPage={{
+          title: "Institutions",
+          href: "#",
+        }}
+      />
+      <LayoutBody>
+        <div className="mx-auto">
+          <SJHeader
+            title="Institutions"
+            description="Manage and monitor list of institutions here"
+          />
+          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+            <Hello />
+            <Stats />
 
-						{/* TODO: display other informative stuffs */}
-						<div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-					</div>
-				</div>
-			</SidebarInset>
-		</SidebarProvider>
-	);
+            {/* TODO: display other informative stuffs */}
+            <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+          </div>
+        </div>
+      </LayoutBody>
+    </SJLayout>
+  );
 }
