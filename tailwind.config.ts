7a0d3adf2d9633b3cb969047ef230a1ee3921a9f
Black Kaiser <khairinchan@bloomthis.co>
2025-01-05 01:24:31 +0800

    fix: 🐛 card dark:hover and buttons hover color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: ["class", "class"],
+	darkMode: ["class"],
 	content: ["./components/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
 	theme: {
     	extend: {
