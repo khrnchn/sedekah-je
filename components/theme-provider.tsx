0eaace8c964608749cdf2be4fac6c66205dcf9aa
Black Kaiser <khairinchan@bloomthis.co>
2025-01-05 01:58:10 +0800

    fix: 🐛 ThemeProvider import

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ThemeProviderProps } from "next-themes/dist/types";
+import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from "next-themes";
 import * as React from "react";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
