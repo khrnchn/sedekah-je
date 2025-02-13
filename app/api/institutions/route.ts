3e7415d256b62ce487b5dea85d089365f571f57d
nrmnqdds <quddusnuriman@gmail.com>
2025-02-13 16:08:33 +0800

    :wheelchair: use fallback data for institutions api

--- app/api/institutions/route.ts
+++ app/api/institutions/route.ts
@@ -1,19 +1,29 @@
 import { getCategories, getCities, getStates } from "@/app/(dashboard)/dashboard/institutions/_lib/queries";
 import { NextResponse } from "next/server";
+import { institutions } from "@/app/data/institutions";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const stateId = searchParams.get("stateId");
 
   try {
-    const [categories, states, cities] = await Promise.all([
+    const result = await Promise.all([
       getCategories(),
       getStates(),
       stateId ? getCities(Number(stateId)) : Promise.resolve([]),
     ]);
 
-    return NextResponse.json({ categories, states, cities });
+    const [categories, states, cities] = result;
+
+    if (result.every((data) => data.length === 0)) {
+      return NextResponse.json(institutions, {
+        status: 200,
+      });
+    }
+
+    return NextResponse.json({ categories, states, cities }, { status: 200 });
   } catch (error) {
+    console.error(error);
     return NextResponse.json(
       { error: "Failed to fetch data" },
       { status: 500 }
