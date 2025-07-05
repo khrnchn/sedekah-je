34d294cb719bc2c0ec4bf8b7fa41d3625746d2ea
Black Kaiser <khairinchan@bloomthis.co>
2025-07-06 00:56:17 +0800

    feat: 🎸 setup betterauth

---
+++ auth.ts
@@ -0,0 +1,18 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "./db";
+
+export const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: "pg",
+	}),
+	emailAndPassword: {
+		enabled: true,
+	},
+	session: {
+		expiresIn: 60 * 60 * 24 * 7, // 7 days
+		updateAge: 60 * 60 * 24, // 1 day
+	},
+	secret: process.env.BETTER_AUTH_SECRET,
+	baseURL: process.env.BETTER_AUTH_URL,
+});
