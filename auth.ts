91155ff9bbec263517cbb3fd544e71a922e196c0
Black Kaiser <khairinchan@bloomthis.co>
2025-07-07 23:19:27 +0800

    feat: 🎸 google

--- auth.ts
+++ auth.ts
@@ -18,9 +18,10 @@ export const auth = betterAuth({
 	},
 	socialProviders: {
 		google: {
-			enabled: true,
-			clientId: process.env.GOOGLE_CLIENT_ID || "",
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+			// always ask the user to select an account
+			prompt: "select_account",
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 		},
 	},
 	session: {
